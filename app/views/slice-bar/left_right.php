a52320b8d4c248d248325cd088ed5a81f358bea8
hau <nguyenviethau.it.2004@gmail.com>
2025-05-08 10:37:37 +0700

    Update left_right.php

--- app/views/slice-bar/left_right.php
+++ app/views/slice-bar/left_right.php
@@ -1,16 +1,23 @@
-<div class="position-fixed top-50 start-0 translate-middle-y d-none d-lg-block z-3" style="left: 10px;">
-    <a href="#">
-        <img src="<?php echo htmlspecialchars($banner_left['image_url']); ?>" alt="Banner Trái"
-            class="rounded-4 shadow  animate__animated animate__bounce animate__infinite"
-            style="width: 12vw; height: 60vh; object-fit: cover;">
-    </a>
-</div>
+<?php if ($banner_left != null || $banner_right != null): ?>
 
-<!-- Banner cố định bên phải -->
-<div class="position-fixed top-50 end-0 translate-middle-y d-none d-lg-block z-3" style="right: 10px;">
-    <a href="#">
-        <img src="<?php echo htmlspecialchars($banner_right['image_url']); ?>" alt="Banner Phải"
-            class="rounded-4 shadow  animate__animated animate__bounce animate__infinite"
-            style="width: 12vw; height: 60vh; object-fit: cover;">
-    </a>
-</div>
+    <?php if ($banner_left != null): ?>
+        <div class="position-fixed top-50 start-0 translate-middle-y d-none d-lg-block z-3" style="left: 10px;">
+            <a href="#">
+                <img src="<?php echo htmlspecialchars($banner_left['image_url']); ?>" alt="Banner Trái"
+                    class="rounded-4 shadow animate__animated animate__bounce animate__infinite"
+                    style="width: 12vw; height: 60vh; object-fit: cover;">
+            </a>
+        </div>
+    <?php endif; ?>
+
+    <?php if ($banner_right != null): ?>
+        <div class="position-fixed top-50 end-0 translate-middle-y d-none d-lg-block z-3" style="right: 10px;">
+            <a href="#">
+                <img src="<?php echo htmlspecialchars($banner_right['image_url']); ?>" alt="Banner Phải"
+                    class="rounded-4 shadow animate__animated animate__bounce animate__infinite"
+                    style="width: 12vw; height: 60vh; object-fit: cover;">
+            </a>
+        </div>
+    <?php endif; ?>
+
+<?php endif; ?>
