6e97a60bac3cb48e03eb1e1838cb588dc8c0e2d0
NGUYỄN VIỆT HẬU <nguyenviethau.it.2004@gmail.com>
2025-06-12 23:17:26 +0700

    Delete config/config.php

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'GOOGLE_CLIENT_ID' => '121408504037-7img4agoj36kbu39prda1srubakifrbh.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET' => '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',
-    'GOOGLE_REDIRECT_URI' => 'http://localhost:8000/auth/google/callback',
-    'OPENROUTER_API_KEY'=>'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'
-];
